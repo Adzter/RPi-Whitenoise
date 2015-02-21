250076a30fd8c5721029e9fce505f3bb1472f978
Adam Wilson <adamwilson790@aol.co.uk>
2015-02-21 16:02:12 +0000

    Added icon for smartphones, improved play functionality

--- inc/controls/play.php
+++ inc/controls/play.php
@@ -3,6 +3,23 @@
 include('../../settings.php');
 include('../message.php');
 
+// Check that we're outputting on ( hdmi or audiojack )
+// as far as i'm aware these are the only native audio types
+// the Raspberry Pi can handle, I guess there's USB?
+
+// After researching it seems like USB isn't a supported audio
+// type of omxplayer, if you're a developer and want to try add
+// USB through a different media player go for it!
+
+// Check what song we're going to play and put the website directory
+// variable (from settings.php) infront of it so the path is correct
+
+// I'm not posting a success message because it waits for the process
+// to end before sending the success message which means it'll only
+// say 'successfully played' once the song has finished, doesn't really
+// work too well, perhaps I can launch it as a background process 
+// or something.
+
 if (isset( $_POST['audioType']) && isset( $_POST['song'])) {
 	$ssh->exec( "omxplayer -o " . $_POST['audioType'] . " " . $websiteDir . $_POST['song'] );
 } else {
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon-precomposed" href="http://<?php echo $_SERVER['SERVER_ADDR']; ?>/whitenoise/img/touch-icon.png"/>
     <title>RPi White Noise</title>
 
     <!-- Bootstrap -->
